253f35a612134b1b8bac0d39e294674f482423da
mushan <mushan0x0@gmail.com>
2022-03-11 17:01:02 +0800

    chore: 完善jest配置

---
+++ setup.js
@@ -0,0 +1,8 @@
+import { Dimensions } from 'react-native';
+import { jest } from '@jest/globals';
+
+// jest.spyOn(Dimensions, 'get').mockReturnValue({ width: 414, height: 818 });
+// jest.mock('@sentry/react-native', () => ({
+//   init: () => jest.fn(),
+//   addBreadcrumb: () => jest.fn(),
+// }));
