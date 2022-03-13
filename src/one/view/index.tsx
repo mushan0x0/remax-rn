8883614ad4882bb84d7758ecbc89ec5986d6ff47
mushan <mushan0x0@gmail.com>
2022-03-13 21:50:32 +0800

    fix: ref

--- src/one/view/index.tsx
+++ src/one/view/index.tsx
@@ -3,7 +3,10 @@ import React, {
   createContext,
   useContext,
   CSSProperties,
+  forwardRef,
   useCallback,
+  useRef,
+  useImperativeHandle,
 } from 'react';
 import {
   View,
@@ -23,7 +26,7 @@ export const extendStyle = createContext(undefined as any as CSSProperties);
 
 const win = Dimensions.get('window');
 
-const ChildrenWrap = ({ children }: any) => {
+const ChildrenWrap = forwardRef(({ children }: any, ref: any) => {
   const isSrt = ['string', 'number'].includes(typeof children);
   const {
     fontSize = rpxToPx(26),
@@ -40,6 +43,7 @@ const ChildrenWrap = ({ children }: any) => {
       wrap={Text}
       wrapProps={{
         className: styles.view,
+        ref,
         style: {
           textAlignVertical: 'center',
           paddingTop:
@@ -61,147 +65,167 @@ const ChildrenWrap = ({ children }: any) => {
       {children}
     </NeedWrap>
   );
-};
+});
 
 export default React.memo(
-  ({
-    children,
-    onTap,
-    onPress,
-    style,
-    ['active-opacity']: activeOpacity = true,
-    ['is-text']: isText = false,
-    className = {},
-    onLayout,
-  }: any) => {
-    const parentStyle = useContext(extendStyle);
-    style = style instanceof Array ? Object.assign({}, ...style) : style;
-    style = {
-      ...className,
-      ...style,
-    };
-    Object.keys(style).forEach((key) => {
-      if (style[key] === undefined) {
-        delete style[key];
-        return;
-      }
-      if (key === 'lineHeight' && +style[key] && +style[key] < 10) {
-        if (+style[key] === 1) {
-          style[key] = +(style.fontSize || parentStyle?.fontSize || 16);
-        } else {
-          style[key] =
-            +style[key] *
-            +(style.fontSize || parentStyle?.fontSize || 16) *
-            1.1;
+  forwardRef(
+    (
+      {
+        children,
+        onTap,
+        onPress,
+        style,
+        ['active-opacity']: activeOpacity = true,
+        ['is-text']: isText = false,
+        className = {},
+        onLayout,
+      }: any,
+      ref
+    ) => {
+      const parentStyle = useContext(extendStyle);
+      style = style instanceof Array ? Object.assign({}, ...style) : style;
+      style = {
+        ...className,
+        ...style,
+      };
+      Object.keys(style).forEach((key) => {
+        if (style[key] === undefined) {
+          delete style[key];
+          return;
         }
-      }
-      if (/em/i.test(style[key])) {
-        const number = +style[key].replace(/em/i, '') * 1.1;
-        style[key] =
-          number *
-          (+(typeof style.fontSize
-            ? style.fontSize
-            : (parentStyle as any)?.fontSize) || rpxToPx(26));
-      }
-    });
-    style = transformStyles(style);
-    children = useMemo(
-      () =>
-        children instanceof Array ? (
-          children.map((item, i) => <ChildrenWrap key={i}>{item}</ChildrenWrap>)
-        ) : (
-          <ChildrenWrap>{children}</ChildrenWrap>
-        ),
-      [children]
-    );
-    style = useMemo(() => {
-      const obj = style instanceof Array ? Object.assign({}, ...style) : style;
-      if (['none', 0].includes(obj?.borderBottom)) {
-        obj.borderBottomWidth = 0;
-        delete obj.borderBottom;
-      }
-      if (['none', 0].includes(obj?.borderTop)) {
-        obj.borderTopWidth = 0;
-        delete obj.borderTop;
-      }
-      const { borderRadius } = obj || {};
-      return transformStyles({
-        flexDirection: parentStyle === undefined ? 'column' : 'row',
-        ...obj,
-        borderRadius: borderRadius?.includes?.('%')
-          ? (((!(parentStyle as any)?.width?.includes?.('%') &&
-              (parentStyle as any)?.width) ||
-              win.width) *
-              +borderRadius.replace('%', '')) /
-            100
-          : borderRadius,
+        if (key === 'lineHeight' && +style[key] && +style[key] < 10) {
+          if (+style[key] === 1) {
+            style[key] = +(style.fontSize || parentStyle?.fontSize || 16);
+          } else {
+            style[key] =
+              +style[key] *
+              +(style.fontSize || parentStyle?.fontSize || 16) *
+              1.1;
+          }
+        }
+        if (/em/i.test(style[key])) {
+          const number = +style[key].replace(/em/i, '') * 1.1;
+          style[key] =
+            number *
+            (+(typeof style.fontSize
+              ? style.fontSize
+              : (parentStyle as any)?.fontSize) || rpxToPx(26));
+        }
       });
-    }, [parentStyle, style]);
-    let isFixed = style.position === 'fixed';
-    // console.log(JSON.stringify(style));
-    const render = useMemo(() => {
-      // console.log(+new Date());
-      return (
-        <extendStyle.Provider
-          value={{
-            ...parentStyle,
-            ...style,
-          }}
-        >
-          <NeedWrap
-            wrap={ScrollView}
-            need={['scroll', 'auto'].includes(style?.overflow)}
-            wrapProps={{
-              style: { height: style.height, flex: style.flex },
-              nestedScrollEnabled: true,
+      style = transformStyles(style);
+      const textRef = useRef(null);
+      children = useMemo(
+        () =>
+          children instanceof Array ? (
+            children.map((item, i) => (
+              <ChildrenWrap ref={textRef} key={i}>
+                {item}
+              </ChildrenWrap>
+            ))
+          ) : (
+            <ChildrenWrap ref={textRef}>{children}</ChildrenWrap>
+          ),
+        [children]
+      );
+      style = useMemo(() => {
+        const obj =
+          style instanceof Array ? Object.assign({}, ...style) : style;
+        if (['none', 0].includes(obj?.borderBottom)) {
+          obj.borderBottomWidth = 0;
+          delete obj.borderBottom;
+        }
+        if (['none', 0].includes(obj?.borderTop)) {
+          obj.borderTopWidth = 0;
+          delete obj.borderTop;
+        }
+        const { borderRadius } = obj || {};
+        return transformStyles({
+          flexDirection: parentStyle === undefined ? 'column' : 'row',
+          ...obj,
+          borderRadius: borderRadius?.includes?.('%')
+            ? (((!(parentStyle as any)?.width?.includes?.('%') &&
+                (parentStyle as any)?.width) ||
+                win.width) *
+                +borderRadius.replace('%', '')) /
+              100
+            : borderRadius,
+        });
+      }, [parentStyle, style]);
+      let isFixed = style.position === 'fixed';
+      const viewRef = useRef(null);
+      useImperativeHandle(
+        ref,
+        () => ({ view: viewRef.current, text: textRef.current }),
+        []
+      );
+      // console.log(JSON.stringify(style));
+      const render = useMemo(() => {
+        // console.log(+new Date());
+        return (
+          <extendStyle.Provider
+            value={{
+              ...parentStyle,
+              ...style,
             }}
           >
             <NeedWrap
-              need={style?.filter?.includes('grayscale')}
-              wrap={Grayscale}
+              wrap={ScrollView}
+              need={['scroll', 'auto'].includes(style?.overflow)}
+              wrapProps={{
+                style: { height: style.height, flex: style.flex },
+                nestedScrollEnabled: true,
+              }}
             >
               <NeedWrap
-                wrap={
-                  onTap || onPress ? TouchableOpacity : isText ? Text : View
-                }
-                need
-                wrapProps={{
-                  className: styles.view,
-                  onLayout,
-                  onPress: (e: any) => {
-                    onTap?.(e);
-                    onPress?.(e);
-                  },
-                  activeOpacity: activeOpacity ? 0.5 : 1,
-                  style: {
-                    ...style,
-                    opacity: style.opacity,
-                    position: isFixed ? 'absolute' : style.position,
-                  },
-                  pointerEvents: style.pointerEvents,
-                }}
+                need={style?.filter?.includes('grayscale')}
+                wrap={Grayscale}
               >
-                {children}
+                <NeedWrap
+                  wrap={
+                    onTap || onPress ? TouchableOpacity : isText ? Text : View
+                  }
+                  need
+                  wrapProps={{
+                    ref: viewRef,
+                    className: styles.view,
+                    onLayout,
+                    onPress: (e: any) => {
+                      onTap?.(e);
+                      onPress?.(e);
+                    },
+                    activeOpacity: activeOpacity ? 0.5 : 1,
+                    style: {
+                      ...style,
+                      opacity: style.opacity,
+                      position: isFixed ? 'absolute' : style.position,
+                    },
+                    pointerEvents: style.pointerEvents,
+                  }}
+                >
+                  {children}
+                </NeedWrap>
               </NeedWrap>
             </NeedWrap>
-          </NeedWrap>
-        </extendStyle.Provider>
+          </extendStyle.Provider>
+        );
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+      }, [
+        activeOpacity,
+        children,
+        isFixed,
+        isText,
+        // onLayout,
+        // onPress,
+        // onTap,
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+        JSON.stringify(parentStyle),
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+        JSON.stringify(style),
+      ]);
+      usePortal(
+        useCallback(() => (isFixed ? render : <></>), [isFixed, render])
       );
-      // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [
-      activeOpacity,
-      children,
-      isFixed,
-      isText,
-      // onLayout,
-      // onPress,
-      // onTap,
-      // eslint-disable-next-line react-hooks/exhaustive-deps
-      JSON.stringify(parentStyle),
-      // eslint-disable-next-line react-hooks/exhaustive-deps
-      JSON.stringify(style),
-    ]);
-    usePortal(useCallback(() => (isFixed ? render : <></>), [isFixed, render]));
-    return isFixed ? null : render;
-  }
+      return isFixed ? null : render;
+    }
+  )
 );
