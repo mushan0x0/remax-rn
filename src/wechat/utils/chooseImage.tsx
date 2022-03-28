50d67b5ead7f792634cc0d69a738198fcf58edd3
mushan <mushan0x0@gmail.com>
2022-03-28 10:32:27 +0800

    fix: ios上传

--- src/wechat/utils/chooseImage.tsx
+++ src/wechat/utils/chooseImage.tsx
@@ -1,5 +1,6 @@
 import ImagePicker from 'react-native-image-crop-picker';
 import { ActionSheet } from '@ant-design/react-native';
+import { Platform } from 'react-native';
 
 export default ({ sourceType }: WechatMiniprogram.ChooseImageOption) => {
   return new Promise((resolve, reject) => {
@@ -21,12 +22,10 @@ export default ({ sourceType }: WechatMiniprogram.ChooseImageOption) => {
           for (let item of images) {
             result.tempFilePaths.push(item.path);
             result.tempFiles.push({
-              path: item.path,
+              uri: Platform.OS === 'ios' ? `file:///${item.path}` : item.path,
               type: item.mime,
-              size: item.size,
-              name: item.filename,
-              width: item.width,
-              height: item.height,
+              name: item.path.split('/').pop(),
+              ...item,
             } as any);
           }
           resolve(result);
